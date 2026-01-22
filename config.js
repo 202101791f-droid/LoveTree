d137e919a82c433c6d491f37b307a0b456b79515
202101791f-droid <202101791f@utea.edu.pe>
2026-01-22 14:47:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-        var config = {
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // Las siguientes oraciones no necesariamente tienen que ser 7; puedes cambiarlas a 4 o incluso a 2 oraciones.
     lines: [
-        "Para el amor de mi vida Yasmina ",
+        "Para el amor de mi vida Yasmina",
         "Si pudiera elegir un lugar",
         "Seguro, seria a tu lado",
         "Cuanto mas tiempo estoy",
@@ -8,9 +10,13 @@
         "Mi querido amorcito, eres mi único amor",
         "Te amaré para siempre...",
     ],
-    // Mi amor comenzo por ti hace....
-    // El formato debe coincidir exactamente con el formato de tiempo que se indica a continuación.
-    // Año-Mes-Día Hora:Minuto:Segundo
-    date: "2017-10-07 00:00:00",
-    //  Nombres de ambas partes
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2017-10-07T00:00:00",
+    // Nombres de ambas partes.
     names: ["Chucurrumi", "Mi tesorote hermsote"]
+}
